94e9a6c1d6886ff6774cdf9be6a88e15aee87f44
Thomas-Fortier <thomasfortier.cs@gmail.com>
2023-07-01 15:42:22 -0400

    Reset files

--- Renessance.Emulator/Hardware/InstructionAttributes.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Renessance.Emulator.Hardware;
-
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-public class InstructionAttributes : Attribute
-{
-  public byte Opcode { get; set; }
-  public AddressingMode Mode { get; set; }
-  public int Cycles { get; set; }
-}
--- Renessance.Emulator/Program.cs
+++ Renessance.Emulator/Program.cs
@@ -5,10 +5,5 @@ internal static class Program
   private static void Main()
   {
     Console.WriteLine("Renessance.Emulator");
-
-    /*var memory = new RAM(new byte[1024 * 64]);
-    var processor = new CPU(memory);
-    
-    processor.ExecuteNextInstruction(1);*/
   }
 }
